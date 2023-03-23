9df735410c0cecac625d8c85a09704d4c7a6a311
Ballphantom <nokaraza012@gmail.com>
2023-03-23 15:00:48 +0700

    add node module

--- calculator.ts
+++ calculator.ts
@@ -5,8 +5,8 @@ const controller = new CalculatorController();
 let argv = process.argv.slice(2)
 
 const num1 = parseFloat(argv[0]);
-const num2 = parseFloat(argv[1]);
-const operator = argv[2];
+const num2 = parseFloat(argv[2]);
+const operator = argv[1];
 
 
 controller.calculate(num1, num2, operator);
