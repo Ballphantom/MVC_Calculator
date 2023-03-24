5cee06508cc4240815bb9882155bdece2267dbdd
Ballphantom <nokaraza012@gmail.com>
2023-03-24 15:42:39 +0700

    comment log

--- src/calculator.ts
+++ src/calculator.ts
@@ -7,12 +7,12 @@ let num1: number, num2: number, operator: string;
 process.stdout.write("Enter num1: ");
 process.stdin.once("data", (data) => {
   num1 = parseFloat(data.toString().trim());
-  console.log(num1);
+  //console.log(num1);
 
   process.stdout.write("Enter num2: ");
   process.stdin.once("data", (data) => {
     num2 = parseFloat(data.toString().trim());
-    console.log(num2);
+    //console.log(num2);
 
     process.stdout.write("Enter operator: ");
     process.stdin.once("data", (data) => {
